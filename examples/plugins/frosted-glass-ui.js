8f884cf00a5a15f1d739cb3e41f9801d83d598ff
laohei <hi@laohei.vip>
2021-12-25 17:41:59 +0800

    fix

--- examples/plugins/frosted-glass-ui.js
+++ examples/plugins/frosted-glass-ui.js
@@ -7,23 +7,9 @@ import Vue from 'vue'
 import 'frosted-glass-ui/lib/styles/frosted-glass-ui.css'
 
 import {
-    AbcTest,
-    BcdTest
+    Button
 } from 'frosted-glass-ui'
 // var FrostedGlassUI = require('frosted-glass-ui')
 // Vue.use(FrostedGlassUI)
 
-// import AbcTest from 'frosted-glass-ui/lib/abc-test.js'
-// import 'frosted-glass-ui/lib/styles/abc-test.css'
-
-// import BcdTest from 'frosted-glass-ui/lib/bcd-test.js'
-// import 'frosted-glass-ui/lib/styles/bcd-test.css'
-
-// var AbcTest = require('frosted-glass-ui/lib/abc-test.js')
-// require('frosted-glass-ui/lib/styles/abc-test.css')
-
-// var BcdTest = require('frosted-glass-ui/lib/bcd-test')
-// require('frosted-glass-ui/lib/styles/bcd-test.css')
-
-Vue.use(AbcTest)
-Vue.use(BcdTest)
+Vue.use(Button)
--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -3,14 +3,12 @@
 		<img alt="Vue logo" src="../assets/logo.png">
 		<br />
 
-		<fg-abc-test>基础</fg-abc-test>
-		<fg-abc-test type="primary">正常</fg-abc-test>
-		<fg-abc-test type="info">信息</fg-abc-test>
-		<fg-abc-test type="success">成功</fg-abc-test>
-		<fg-abc-test type="warning">警告</fg-abc-test>
-		<fg-abc-test type="error">错误</fg-abc-test>
-
-		<fg-bcd-test></fg-bcd-test>
+		<fg-button>基础</fg-button>
+		<fg-button type="primary">正常</fg-button>
+		<fg-button type="info">信息</fg-button>
+		<fg-button type="success">成功</fg-button>
+		<fg-button type="warning">警告</fg-button>
+		<fg-button type="error">错误</fg-button>
 
 	</div>
 </template>
